75ff3ea5c57da32a0c4a19cd4cc524d317bfaef6
pablo <pablobronzeado@outlook.com>
2025-04-01 13:48:46 -0300

    criando o componente pg-principal e adicionando o menu de opçoes para o usuario navegar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { PgPrincipalComponent } from "./pg-principal/pg-principal.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, PgPrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/pg-principal/pg-principal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pg-principal',
+  imports: [],
+  templateUrl: './pg-principal.component.html',
+  styleUrl: './pg-principal.component.scss'
+})
+export class PgPrincipalComponent {
+
+}
